6bc4e75f9a43f3116ddadcc11d75b2dee9cfa80d
Richuan_Li <rc620706@dal.ca>
2024-10-09 19:28:31 -0300

    reset app

---
+++ frontend/app/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is the homepage</Text>
+    </View>
+  );
+}
