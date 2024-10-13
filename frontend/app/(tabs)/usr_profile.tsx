21e5449a4987e4e012f36e4455805b8bdbf54fa2
rgass <ry967465@dal.ca>
2024-10-12 23:24:29 -0300

    Created initial profile page

--- frontend/app/(tabs)/usr_profile.tsx
+++ frontend/app/(tabs)/usr_profile.tsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native';
 
 const usr_profile = () => {
+    const displayImage = () => {
+        return(
+            <Image
+                style={styles.logo}
+                source={require('../../assets/images/icon.png')}
+            />
+        );
+    }
+
   return (
-    <View>
-      <Text>usr_profile</Text>
-    </View>
+    <ScrollView>
+        <View>
+            <View style={{flexDirection: "row"}}>
+                <Image
+                    style={{
+                        width: 150,
+                        height: 150,
+                        }}
+                    source={require('../../assets/images/icon.png')}
+                />
+                <View>
+                    <Text style={{fontSize: "25px", marginLeft: 10}}>Name: </Text>
+                    <Text style={{fontSize: "25px", marginLeft: 10}}>Phone: </Text>
+                </View>
+            </View>
+            <Text style={{marginLeft: 10, marginTop: 10}}>University Name: </Text>
+            <Text style={{marginLeft: 10}}>Major: </Text>
+            <Text style={{marginLeft: 10}}>Year: </Text>
+            <Text style={{marginLeft: 10}}>Study Preference: </Text>
+            <Text style={{marginLeft: 10}}>Availability: </Text>
+        </View>
+    </ScrollView>
   )
 }
 
