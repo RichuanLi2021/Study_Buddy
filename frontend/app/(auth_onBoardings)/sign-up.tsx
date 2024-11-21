06653d2ab7e89fd0d1be4f6f09d9cd5c402cfbdd
Tyler <ty868750@dal.ca>
2024-11-21 13:02:47 -0400

    added index functionality and flow

--- frontend/app/(auth_onBoardings)/sign-up.tsx
+++ frontend/app/(auth_onBoardings)/sign-up.tsx
@@ -1,82 +1,83 @@
-import { View, Text, ScrollView, Alert} from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import FormSignup from '@/components/auth/forSignup/FormSignup'
-import CustomButton from '@/components/auth/button/buttons'
-import { validateEmail, validatePassword } from '@/components/auth/InputValidation/Input_validation'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '@/config/firebaseConfig'
+import { View, Text, ScrollView, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FormSignup from '@/components/auth/forSignup/FormSignup';
+import CustomButton from '@/components/auth/button/buttons';
+import { validateEmail, validatePassword } from '@/components/auth/InputValidation/Input_validation';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/config/firebaseConfig';
+import { useRouter } from 'expo-router'; // Import useRouter for navigation
+import { Link, router } from 'expo-router';
 
 const Signup = () => {
+  const router = useRouter(); // Initialize the router
 
   const [value, setValue] = useState({
-      email: "",
-      password: "",
-      name: "",
-      university: "",
-      major: "",
-      year: "",
-      studyPreference: "",
-      error: "",
+    email: '',
+    password: '',
+    name: '',
+    university: '',
+    major: '',
+    year: '',
+    studyPreference: '',
+    error: '',
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-
   const handleSignupPress = async () => {
     // Handle verification logic here
-    if(value.email === null && !validateEmail(value.email)){
-      Alert.alert("Invalid Email format");
+    if (value.email === null && !validateEmail(value.email)) {
+      Alert.alert('Invalid Email format');
       return;
     }
 
-    if(value.password ===null && !validatePassword(value.password)){
-      Alert.alert("Invalid Password");
+    if (value.password === null && !validatePassword(value.password)) {
+      Alert.alert('Invalid Password');
       return;
     }
 
     setIsSubmitting(true);
 
     try {
-      //Sign up user
+      // Sign up user
       await createUserWithEmailAndPassword(auth, value.email, value.password)
-      .then((userCredential)=> {
-        const user = userCredential.user;
-        console.log("Hello! :" + user)
-        Alert.alert("You are registered successfully")
-      
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setValue({...value, error: errorMessage})
-        Alert.alert("Sorry, something went wrong with your signup: " + value.error);
-      });
-
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log('Hello! : ' + user);
+          Alert.alert('You are registered successfully');
+          router.push('/usr_home'); // Redirect to usr_home
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setValue({ ...value, error: errorMessage });
+          Alert.alert('Sorry, something went wrong with your signup: ' + value.error);
+        });
     } catch (error) {
       setValue({
         ...value,
         error: error instanceof Error ? error.message : 'An unknown error occurred',
-    });
-    
-    }finally {
+      });
+    } finally {
       // Stop submitting
       setIsSubmitting(false);
     }
-  }
+  };
 
   return (
     <SafeAreaView
-      className='bg-orange-50 h-full'
+      className="bg-orange-50 h-full"
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10
-      }}>
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+      }}
+    >
       <ScrollView>
-        <View className='w-full justify-center h-full px-4 my-6'>
-          <Text 
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Text
             className="text-2xl text-cyan-900 font-semibold mt-10"
             style={{
               marginBottom: 20,
@@ -84,106 +85,131 @@ const Signup = () => {
               fontSize: 28,
               color: '#FF7900',
               fontWeight: 700,
-            }}> 
+            }}
+          >
             Signup with your Email
           </Text>
 
           {/* Enter name */}
           <FormSignup
-            title='Name'
+            title="Name"
             value={value.name}
             handleChangeText={(usrName) => {
               setValue({
-                ...value, name: usrName
-              })
+                ...value,
+                name: usrName,
+              });
             }}
-            keyboardType='default'
+            keyboardType="default"
           />
 
           {/* Enter university */}
           <FormSignup
-            title='University'
+            title="University"
             value={value.university}
             handleChangeText={(usrUniversity) => {
               setValue({
-                ...value, university: usrUniversity
-              })
+                ...value,
+                university: usrUniversity,
+              });
             }}
-            keyboardType='default'
+            keyboardType="default"
           />
 
           {/* Enter major */}
           <FormSignup
-            title='Major'
+            title="Major"
             value={value.major}
             handleChangeText={(usrMajor) => {
               setValue({
-                ...value, major: usrMajor
-              })
+                ...value,
+                major: usrMajor,
+              });
             }}
-            keyboardType='default'
+            keyboardType="default"
           />
 
           {/* Enter year */}
           <FormSignup
-            title='Year of Study'
+            title="Year of Study"
             value={value.year}
             handleChangeText={(usrYearOfStudy) => {
               setValue({
-                ...value, year: usrYearOfStudy
-              })
+                ...value,
+                year: usrYearOfStudy,
+              });
             }}
-            keyboardType='numeric'
+            keyboardType="numeric"
           />
 
           {/* Enter study preference */}
           <FormSignup
-            title='Study Preference'
+            title="Study Preference"
             value={value.studyPreference}
             handleChangeText={(usrPreference) => {
               setValue({
-                ...value, studyPreference: usrPreference
-              })
+                ...value,
+                studyPreference: usrPreference,
+              });
             }}
-            keyboardType='default'
+            keyboardType="default"
           />
 
           {/* Enter email */}
           <FormSignup
-            title='Email'
+            title="Email"
             value={value.email}
             handleChangeText={(usrEmail) => {
               setValue({
-                ...value, email: usrEmail
-              })
+                ...value,
+                email: usrEmail,
+              });
             }}
-            keyboardType='email-address'
+            keyboardType="email-address"
           />
 
           {/* Enter password */}
           <FormSignup
-            title='Password'
+            title="Password"
             value={value.password}
             handleChangeText={(usrPassword) => {
               setValue({
-                ...value, password: usrPassword
-              })
+                ...value,
+                password: usrPassword,
+              });
             }}
-            keyboardType='default'
+            keyboardType="default"
           />
-
 
           {/* Verify email Button */}
           <CustomButton
-            title='Create Account'
+            title="Create Account"
             handlePress={handleSignupPress}
             buttonStyle={{ marginTop: 20 }}
-            isLoading={false}
+            isLoading={isSubmitting}
           />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+              <Text className='text-base text-red-950 font-normal'>
+                  Already have an account?
+                     </Text>
+                       <Link
+                          href='/(auth_onBoardings)/sign-in'
+                          className='text-base font-extrabold text-orange-500'
+                            style={{
+                                color: 'blue',
+                                fontWeight: 600,
+                                textDecorationLine: 'underline',
+
+                                }}>
+                            Log in
+                       </Link>
+          </View>
         </View>
+
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
